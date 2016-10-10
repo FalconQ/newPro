568035d2707d566612b6e3d3a9dee922381abfb9
fy <183168689@qq.com>
2016-10-10 21:51:49 +0800

    add

--- components/mine/MineController.js
+++ components/mine/MineController.js
@@ -1,7 +1,7 @@
 /**
  * Created by qiusheng on 2016/10/9.
  */
-define(['router'],function (app) {
+define(['router','$css!./components/mine/mine.css'],function (app) {
     // angular会自动根据controller函数的参数名，导入相应的服务
     return app.controller('MineController',['$scope',function ($scope) {
         $scope.info = '我的';
