67b275db0ccd2b89ce56254a9331a655c8d477c0
18686852547 <945518333@qq.com>
2016-10-10 19:48:43 +0800

    addhome.html

--- components/home/HomeController.js
+++ components/home/HomeController.js
@@ -1,7 +1,7 @@
 /**
  * Created by qiusheng on 2016/10/9.
  */
-define(['router','$css!./home.css'],function (app) {
+define(['router','$css!./components/home/home.css'],function (app) {
     // angular会自动根据controller函数的参数名，导入相应的服务
     return app.controller('HomeController',['$scope',function ($scope) {
         $scope.info = '测试';
