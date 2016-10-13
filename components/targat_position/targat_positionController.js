0749b47a60fe76686649424f2c88620baee6fa70
caohefie <1187927979@qq.com>
2016-10-13 20:57:13 +0800

    add

--- components/targat_position/targat_positionController.js
+++ components/targat_position/targat_positionController.js
@@ -6,7 +6,14 @@
  */
 define(['router',"$css!./components/targat_position/targat_position.css"],function (app) {
     // angular会自动根据controller函数的参数名，导入相应的服务
-    return app.controller('targat_positionController',['$scope',function ($scope) {
+    return app.controller('targat_positionController',['$scope','$http',function ($scope,$http) {
+               $http.get('./data/beijing.json')
+                   .success(function(data){
+                        console.log(data.response.resultList.record)
+                       $scope.objs_1=data.response.resultList.record;
+                       $scope.CityName=data.response.ipLoc.CityName;
+                   })
+
 
     }])
 })
