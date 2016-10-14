8ba8f09454d609b2080d250fede4bf872e624921
caohefie <1187927979@qq.com>
2016-10-14 16:27:04 +0800

    add

--- components/target/targetController.js
+++ components/target/targetController.js
@@ -3,7 +3,10 @@
  */
 define(['router',"$css!./components/target/target.css"],function (app) {
     // angular会自动根据controller函数的参数名，导入相应的服务
-    return app.controller('targetController',['$scope',function ($scope) {
-        $scope.info = '曹合飞';
+    return app.controller('targetController',['$scope','$http',function ($scope, $http) {
+                $http.get('./data/quanguo.json')
+                     .success(function(data){
+                         console.log(data)
+                    })
     }])
 })
