351d16777c539f879bb97330e0db6e4f3f5dc10b
caohefie <1187927979@qq.com>
2016-10-14 21:01:36 +0800

    add

--- components/target/targetController.js
+++ components/target/targetController.js
@@ -4,19 +4,44 @@
 define(['router',"$css!./components/target/target.css"],function (app) {
     // angular会自动根据controller函数的参数名，导入相应的服务
     return app.controller('targetController',['$scope','$http',function ($scope, $http) {
-                $http.get('./data/hot.json')
+              // pag1获取右边内容的数据生成
+              $http.get('./data/hot.json')
                      .success(function(data){
-                         $scope.obj1=data.response.groupList[1].cellItem
+                         $scope.obj1=data.response.groupList[1].cellItem;
                     });
-                $http.get('./data/tabdata.json')
+
+               //page2 获取右边数据内容生成
+               $http.get("./data/zhoubian.json")
+                   .success(function(data){
+                       console.log(data.response.body.groupList[0].cellItem);
+                       $scope.obj2=data.response.body.groupList[0].cellItem
+
+                   })
+              //获取左边tabar数据生成tabar
+              $http.get('./data/tabdata.json')
                     .success(function(data){
                         $scope.obj2=data.categoryList;
                     });
-        angular.element(".modlTargat_left ul").find("li").on("click",function(){
-            var eleLIi=event.target;
-            $(eleLIi).css({"background":"white","color":"#6ad3c8"}).siblings().css({"background":"none","color":"black"})
-        })
+              //点击事件的设置
+              $scope.set=function(){
+                      $(event.target).css({"background":"white","color":"#6ad3c8"}).siblings().css({"background":"none","color":"black"})
+                       app.directive("myPage",function(){
+                          console.log("ddd")
+                          return {
+                              restrict:"E",
+                              templateUrl:"./components/target/page_zhoubian.html",
+                              replace:true
+                          }
+                      })
+              }
+             }])
+             .directive("myPage",function(){
+                    return {
+                        restrict:"E",
+                        templateUrl:"./components/target/page_zhoubian.html",
+                        replace:true
+                    }
+             })
 
-    }])
 
 })
